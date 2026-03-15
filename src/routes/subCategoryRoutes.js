7fa7e361907dc2b35c53b0d2ddc3335d4b7a2e1a
Pranaykhetade02 <pranaykhetade152@gmail.com>
2026-03-15 16:58:52 +0530

    getsubcategorybyid api is done

--- src/routes/subCategoryRoutes.js
+++ src/routes/subCategoryRoutes.js
@@ -1,9 +1,9 @@
 import express from "express"
-import { createSubCategory,getAllsubcategory,deleteSubcategory } from "../controller/subcategoryController.js"
+import { createSubCategory,getAllsubcategory,deleteSubcategory,getByIdSubcategory } from "../controller/subcategoryController.js"
 const Router = express.Router()
 
 Router.post("/create-subcategory",createSubCategory)
 Router.get("/getAllsubcategory",getAllsubcategory)
 Router.delete("/deletesubcategory/:id",deleteSubcategory)
-
+Router.get("/getByIdSubcategory/:id",getByIdSubcategory)
 export default Router
